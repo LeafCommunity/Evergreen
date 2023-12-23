ef310cffa4c59e68e8b5e32d03bb6d4022b67b98
RezzedUp <RezzedUp@users.noreply.github.com>
2023-12-22 22:29:04 -0600

    Update license headers.

--- bukkit-server-version/src/main/java/community/leaf/evergreen/bukkit/versions/MinecraftVersion.java
+++ bukkit-server-version/src/main/java/community/leaf/evergreen/bukkit/versions/MinecraftVersion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022, RezzedUp <https://github.com/LeafCommunity/Evergreen>
+ * Copyright © 2022-2023, RezzedUp <https://github.com/LeafCommunity/Evergreen>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- bukkit-server-version/src/test/java/community/leaf/evergreen/bukkit/versions/CraftBukkitVersionTests.java
+++ bukkit-server-version/src/test/java/community/leaf/evergreen/bukkit/versions/CraftBukkitVersionTests.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2022-2023, RezzedUp <https://github.com/LeafCommunity/Evergreen>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package community.leaf.evergreen.bukkit.versions;
 
 import org.junit.jupiter.api.Test;
